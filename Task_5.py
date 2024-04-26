e0e8f13ec3a3a662e69b5308c3ea427ca6fe0c78
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-04-26 00:16:48 -0400

    Task 5 started

---
+++ Task_5.py
@@ -0,0 +1,5 @@
+# The Fitness Tracker App
+
+# Task 1: Create a func that logs activities and their duration
+# Task 2: Create a func that provides the est. calories burned based on the activity and duration
+# Task 3: Create a func that provides a summary
