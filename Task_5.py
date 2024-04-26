d96311d290a0cd395fd568d717df0fec4be7b66e
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-04-26 15:51:41 -0400

    Task 5 WIP

--- Task_5.py
+++ Task_5.py
@@ -3,3 +3,12 @@
 # Task 1: Create a func that logs activities and their duration
 # Task 2: Create a func that provides the est. calories burned based on the activity and duration
 # Task 3: Create a func that provides a summary
+
+def activityLog():
+    pass
+
+def burntCal():
+    pass
+
+def activitysummary():
+    pass
